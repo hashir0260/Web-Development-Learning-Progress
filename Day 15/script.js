c175b89e7fb8390943af42575d937fe27135b60b
hashir0260 <hashirali0260@gmail.com>
2025-07-29 23:01:48 +0500

    Learned Let,const and var , variable declaration, printing statement in javascript, printing in tablular form

---
+++ Day 15/script.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "hashir21@gmail.com"
+var accountPassword = "3456"
+accountCity = "Wahcantt"
+
+accountEmail = "hashir65@gmail.com"
+accountPassword = "456"
+accountCity = "Lahore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
