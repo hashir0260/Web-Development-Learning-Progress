4accdafe1bf6695297fc33d6b1cebee8e1617e47
hashir0260 <hashirali0260@gmail.com>
2025-08-01 01:40:57 +0500

    Learned about Stack and Heap in javascript

---
+++ Day 18/HeapAndStack.js
@@ -0,0 +1,25 @@
+//Stack Example
+
+let name = "Hashir"
+
+newName = name;
+
+newName = "HashirCoder";
+
+console.log(name);
+console.log(newName);
+
+// Heap Example
+
+let person = {
+    email: "hashirali21@yahoo.com"
+}
+
+let person2 = person;
+
+person2.email = "hashirhehe30@gmail.com";
+
+console.log(person.email);
+console.log(person2.email);
+
+
