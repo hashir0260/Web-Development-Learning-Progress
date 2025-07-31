f76a1ed479144a6e4e8c247991f2da3d07d9b5fb
hashir0260 <hashirali0260@gmail.com>
2025-07-31 23:28:37 +0500

    Learned prefix and postfix, Type conversion from string to number

---
+++ Day 16/Prefix.js
@@ -0,0 +1,13 @@
+let x = 3;
+let y = x++;
+
+console.log(x , y);
+
+// postfix
+
+let a = 3;
+let b = ++a;
+
+console.log(a , b);
+
+console.log(`My age is ${a}`);
---
+++ Day 17/script.js
@@ -0,0 +1,14 @@
+let num1 = "22"
+let newNumber = Number(num1);
+console.log(newNumber);
+console.log(typeof newNumber);
+
+let score = true;
+console.log(typeof score);
+
+console.log(score);
+
+let newScore = Number(score);
+console.log(newScore);
+console.log(typeof newScore);
+
