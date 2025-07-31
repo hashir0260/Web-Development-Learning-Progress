7f7d9ff7c351277038144dc4136348fa01028b7b
hashir0260 <hashirali0260@gmail.com>
2025-08-01 00:44:44 +0500

    Comparison of data types

---
+++ Day 18/script.js
@@ -0,0 +1,32 @@
+console.log(true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+let a , b ;
+a = 10;
+b = a++;
+console.log(a , b);
+
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 === 1);
+
+console.log("23" > 20)
+
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(null == undefined);
+
+console.log("2"==1)
+
+console.log(undefined == 0);
+
+console.log(undefined < 0);
+
+console.log("2" == 2);
