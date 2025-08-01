ace08411687063867c3028a7a8227a1ac789ffb6
hashir0260 <hashirali0260@gmail.com>
2025-08-02 00:53:02 +0500

    Learned String functions in Javascript

---
+++ Day 18/Strings.js
@@ -0,0 +1,32 @@
+const name = "Hashir Ali";
+const age = 20;
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+
+const gameName = new String("Chess");
+
+console.log(`I love playing ${gameName}`);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+
+console.log(gameName.charAt(3));
+
+console.log(gameName.indexOf("C"));
+
+console.log(gameName.lastIndexOf("h"));
+
+console.log(gameName.slice(0, 4));
+
+const newName = "  hashir  ";
+
+console.log(newName.trim());
+
+console.log(newName.trimStart());
+
+
+
+
+
+
+
---
+++ Day 19/script.js
@@ -0,0 +1,21 @@
+const name = "Hashir";
+
+const newName = new String(name);
+
+console.log(newName);
+console.log(newName[0]);
+
+
+
+console.log(newName.length);
+
+console.log(newName.toLocaleLowerCase());
+console.log(newName.charAt(3));
+
+console.log(newName.indexOf('s'));
+
+console.log(newName.substring(1, 5));
+
+console.log(newName.slice(-6, 0));
+const st = "  hashir   "
+console.log(st.trim());
