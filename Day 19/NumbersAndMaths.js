3a8f3e55944a50f8910429cb8097e7d08ec60478
hashir0260 <hashirali0260@gmail.com>
2025-08-02 23:17:16 +0500

    Learned Numbers and Maths in javascript

---
+++ Day 19/NumbersAndMaths.js
@@ -0,0 +1,26 @@
+let score = 200;
+
+let newScore = new Number(100);
+
+console.log(newScore);
+
+console.log(newScore.toString());
+
+console.log(newScore.toFixed(4));
+
+console.log(typeof newScore);
+
+const val = 23.37852;
+console.log(val.toPrecision(2));
+
+
+const money = 100000000;
+
+console.log(money.toLocaleString('en-US'));
+
+// Maths
+
+console.log(Math.PI.toFixed(2));
+
+console.log(Math.abs(-3));
+
