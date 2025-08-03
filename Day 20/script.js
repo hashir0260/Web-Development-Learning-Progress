7f50bc132374b8ed24414022d7349acfbf583a29
hashir0260 <hashirali0260@gmail.com>
2025-08-03 21:47:35 +0500

    All Math Functions

---
+++ Day 20/script.js
@@ -0,0 +1,25 @@
+console.log(Math.abs(-4));
+
+console.log(Math.round(2.6));
+
+console.log(Math.ceil(2.2));
+
+console.log(Math.floor(2.9));
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.sqrt(4));
+
+console.log(Math.min(2,9,4,6,3));
+
+console.log(Math.max(9,10,3,6,76));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 5;
+const max = 15;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
